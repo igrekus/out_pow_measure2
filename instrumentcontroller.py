1e36c202c65d27cb0cc7c4a98731694495fdbce0
igrekus <upload234@mail.ru>
2021-12-03 15:41:19 +0300

    Initial commit

---
+++ instrumentcontroller.py
@@ -0,0 +1,363 @@
+import ast
+import time
+
+import numpy as np
+
+from PyQt5.QtCore import QObject, pyqtSlot, pyqtSignal
+from forgot_again.file import load_ast_if_exists, pprint_to_file
+
+from instr.instrumentfactory import mock_enabled, SourceFactory, AnalyzerFactory
+from measureresult import MeasureResult
+from secondaryparams import SecondaryParams
+
+GIGA = 1_000_000_000
+MEGA = 1_000_000
+KILO = 1_000
+MILLI = 1 / 1_000
+
+
+class InstrumentController(QObject):
+    pointReady = pyqtSignal()
+
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        addrs = load_ast_if_exists('instr.ini', default={
+            'Анализатор': 'GPIB1::18::INSTR',
+            'Источник': 'GPIB1::3::INSTR',
+        })
+
+        self.requiredInstruments = {
+            'Анализатор': AnalyzerFactory(addrs['Анализатор']),
+            'Источник': SourceFactory(addrs['Источник']),
+        }
+
+        self.deviceParams = {
+            'ГУН': {
+                'F': 1,
+            },
+        }
+
+        self.secondaryParams = SecondaryParams(required={
+            'sep_4': ['', {'value': None}],
+            'u_src_drift_1': [
+                'Uп=',
+                {'start': 0.0, 'end': 10.0, 'step': 0.5, 'value': 4.7, 'suffix': ' В'}
+            ],
+            'u_src_drift_2': [
+                'Uдр. мин=',
+                {'start': 0.0, 'end': 10.0, 'step': 0.5, 'value': 5.0, 'suffix': ' В'}
+            ],
+            'u_src_drift_3': [
+                'Uдр. макс=',
+                {'start': 0.0, 'end': 10.0, 'step': 0.5, 'value': 5.3, 'suffix': ' В'}
+            ],
+            'i_src_max': [
+                'Iп.макс=',
+                {'start': 0.0, 'end': 500.0, 'step': 1.0, 'value': 50.0, 'suffix': ' мА'}
+            ],
+            'sep_1': ['', {'value': None}],
+            'u_vco_min': [
+                'Uупр.мин.=',
+                {'start': 0.0, 'end': 30.0, 'step': 0.5, 'decimals': 2, 'value': 0.0, 'suffix': ' В'}
+            ],
+            'u_vco_max': [
+                'Uупр.макс.=',
+                {'start': 0.0, 'end': 30.0, 'step': 0.5, 'decimals': 2, 'value': 10.0, 'suffix': ' В'}
+            ],
+            'u_vco_delta': [
+                'ΔUупр=',
+                {'start': 0.0, 'end': 30.0, 'step': 0.5, 'decimals': 2, 'value': 1.0, 'suffix': ' В'}
+            ],
+            'sep_2': ['', {'value': None}],
+            'sa_min': [
+                'SA start=',
+                {'start': 0.0, 'end': 30.0, 'step': 0.5, 'value': 1.0, 'suffix': ' ГГц'}
+            ],
+            'sa_max': [
+                'SA stop=',
+                {'start': 0.0, 'end': 30.0, 'step': 0.5, 'value': 1.0, 'suffix': ' ГГц'}
+            ],
+            'sa_rlev': [
+                'SA ref lev=',
+                {'start': -30.0, 'end': 30.0, 'step': 1.0, 'value': 10.0, 'suffix': ' дБ'}
+            ],
+            'sa_span': [
+                'SA span=',
+                {'start': 0.0, 'end': 30000.0, 'step': 1.0, 'value': 50.0, 'suffix': ' МГц'}
+            ],
+            'sep_3': ['', {'value': None}],
+            'file_name': [
+                'Имя файла=',
+                {'value': 'test', }
+            ],
+            # 'is_harm_relative': [
+            #     'Отн.ур.гармоник',
+            #     {'value': False}
+            # ],
+            # 'is_u_src_drift': [
+            #     'Дрейф от Uп',
+            #     {'value': False}
+            # ],
+        })
+        self.secondaryParams.load_from_config('params.ini')
+
+        self._calibrated_pows_lo = load_ast_if_exists('cal_lo.ini', default={})
+        self._calibrated_pows_mod = load_ast_if_exists('cal_mod.ini', default={})
+        self._calibrated_pows_rf = load_ast_if_exists('cal_rf.ini', default={})
+
+        self._instruments = dict()
+        self.found = False
+        self.present = False
+        self.hasResult = False
+
+        self.result = MeasureResult()
+
+    def __str__(self):
+        return f'{self._instruments}'
+
+    # region connections
+    def connect(self, **kwargs):
+        addrs = kwargs.pop('addrs')
+        fn_progress = kwargs.pop('fn_progress', None)
+
+        print(f'searching for {addrs}')
+        for k, v in addrs.items():
+            self.requiredInstruments[k].addr = v
+        self.found = self._find()
+
+    def _find(self):
+        self._instruments = {
+            k: v.find() for k, v in self.requiredInstruments.items()
+        }
+        return all(self._instruments.values())
+
+    def check(self, token, params):
+        print(f'call check with {token} {params}')
+        device, secondary = params
+        self.present = self._check(token, device, secondary)
+        print('sample pass')
+
+    def _check(self, token, device, secondary):
+        print(f'launch check with {self.deviceParams[device]} {self.secondaryParams}')
+        self._init()
+        return True
+    # endregion
+
+    # region calibrations
+    def calibrate(self, token, params):
+        print(f'call calibrate with {token} {params}')
+        return self._calibrate(token, self.secondaryParams)
+
+    def _calibrateLO(self, token, secondary):
+        print('run calibrate LO with', secondary)
+        result = {}
+        self._calibrated_pows_lo = result
+        return True
+
+    def _calibrateRF(self, token, secondary):
+        print('run calibrate RF')
+        result = {}
+        self._calibrated_pows_rf = result
+        return True
+
+    def _calibrateMod(self, token, secondary):
+        print('calibrate mod gen')
+        result = {}
+        self._calibrated_pows_mod = result
+        return True
+    # endregion
+
+    # region initialization
+    def _clear(self):
+        self.result.clear()
+
+    def _init(self):
+        self._instruments['Источник'].send('*RST')
+        self._instruments['Источник'].send('OUTP OFF')
+        self._instruments['Анализатор'].send('*RST')
+    # endregion
+
+    def measure(self, token, params):
+        print(f'call measure with {token} {params}')
+        device, _ = params
+        try:
+            self.result.set_secondary_params(self.secondaryParams)
+            self._measure(token, device)
+            # self.hasResult = bool(self.result)
+            self.hasResult = True  # TODO HACK
+        except RuntimeError as ex:
+            print('runtime error:', ex)
+
+    def _measure(self, token, device):
+        param = self.deviceParams[device]
+        secondary = self.secondaryParams.params
+        print(f'launch measure with {token} {param} {secondary}')
+
+        self._clear()
+        _, x2, x3 = self._measure_tune(token, param, secondary)
+        self.result.add_harmonics_measurement(x2, x3)
+        self.result.set_secondary_params(self.secondaryParams)
+        return True
+
+    def _measure_tune(self, token, param, secondary):
+
+        def find_peak_read_marker():
+            sa.send("CALC:MARK1:MAX")
+            if not mock_enabled:
+                time.sleep(0.1)
+
+            freq = float(sa.query(":CALC:MARK1:X?"))
+            pow_ = float(sa.query(":CALC:MARK1:Y?"))
+            return freq, pow_
+
+        def measure_harmonics(multiplier):
+            print('measure harmonics:', multiplier)
+            r = []
+            sa.send(f':SENS:FREQ:SPAN {sa_span}')
+            for uc, f in pairs:
+
+                if token.cancelled:
+                    src.send('OUTP OFF')
+                    sa.send(':CAL:AUTO ON')
+                    raise RuntimeError('measurement cancelled')
+
+                src.send(f'APPLY p6v,{u_src_drift_1}V,{i_src_max}A')
+                src.send(f'APPLY p25v,{uc}V,{i_tune_max}A')
+
+                if not mock_enabled:
+                    time.sleep(1)
+
+                f_xmul = f * multiplier
+                sa.send(f':SENS:FREQ:CENT {f_xmul}Hz')
+
+                if not mock_enabled:
+                    time.sleep(0.5)
+
+                sa.send('CALC:MARK1:MAX')
+
+                if not mock_enabled:
+                    time.sleep(0.05)
+
+                read_p = float(sa.query(f'CALC1:MARK1:Y?'))
+
+                point = {
+                    'u_control': uc,
+                    'read_p': read_p,
+                }
+                r.append(point)
+
+            return r
+
+        src = self._instruments['Источник']
+        sa = self._instruments['Анализатор']
+
+        i_src_max = secondary['i_src_max'] * MILLI
+
+        u_tune_min = secondary['u_vco_min']
+        u_tune_max = secondary['u_vco_max']
+        u_tune_step = secondary['u_vco_delta']
+        i_tune_max = 10 * MILLI
+
+        sa_f_start = secondary['sa_min'] * GIGA
+        sa_f_stop = secondary['sa_max'] * GIGA
+        sa_rlev = secondary['sa_rlev']
+        sa_span = secondary['sa_span'] * MEGA
+
+        u_src_drift_1 = secondary['u_src_drift_1']
+        u_src_drift_2 = secondary['u_src_drift_2']
+        u_src_drift_3 = secondary['u_src_drift_3']
+
+        u_control_values = [round(x, 2) for x in np.arange(start=u_tune_min, stop=u_tune_max + 0.002, step=u_tune_step)]
+        u_drift_values = [u for u in [u_src_drift_1, u_src_drift_2, u_src_drift_3] if u]
+
+        # region main measure
+        # TODO set source according to the source model
+        src.send(f'APPLY p6v,{u_src_drift_1}V,{i_src_max}A')
+        src.send(f'APPLY p25v,{u_control_values[0]}V,{i_tune_max}A')
+
+        sa.send(f'DISP:WIND:TRAC:Y:RLEV {sa_rlev}')
+        sa.send(f':SENS:FREQ:STAR {sa_f_start}Hz')
+        sa.send(f':SENS:FREQ:STOP {sa_f_stop}Hz')
+        sa.send(':CAL:AUTO OFF')
+        sa.send(':CALC:MARK1:MODE POS')
+
+        src.send('OUTP ON')
+
+        if mock_enabled:
+            with open('./mock_data/4.7-0-0.txt', mode='rt', encoding='utf-8') as f:
+            # with open('./mock_data/4.7-5.0-5.3.txt', mode='rt', encoding='utf-8') as f:
+                index = 0
+                mocked_raw_data = ast.literal_eval(''.join(f.readlines()))
+
+        result = []
+        for u_drift in u_drift_values:
+            for u_control in u_control_values:
+
+                if token.cancelled:
+                    src.send('OUTP OFF')
+                    sa.send(':CAL:AUTO ON')
+                    raise RuntimeError('measurement cancelled')
+
+                src.send(f'APPLY p6v,{u_drift}V,{i_src_max}A')
+                src.send(f'APPLY p25v,{u_control}V,{i_tune_max}A')
+
+                if not mock_enabled:
+                    time.sleep(1)
+
+                read_f, read_p = find_peak_read_marker()
+                read_i = float(src.query('MEAS:CURR? p6v'))
+
+                raw_point = {
+                    'u_src': u_drift,
+                    'u_control': u_control,
+                    'read_f': read_f,
+                    'read_p': read_p,
+                    'read_i': read_i,
+                }
+
+                print('measured point:', raw_point)
+
+                if mock_enabled:
+                    raw_point = mocked_raw_data[index]
+                    index += 1
+
+                self._add_measure_point(raw_point)
+
+                result.append(raw_point)
+
+        with open('out.txt', mode='wt', encoding='utf-8') as out_file:
+            out_file.write(str(result))
+
+        pairs = [[row['u_control'], row['read_f'] * MEGA] for row in result if row['u_src'] == u_src_drift_1]
+
+        result_harmonics_x2 = measure_harmonics(multiplier=2)
+        result_harmonics_x3 = measure_harmonics(multiplier=3)
+
+        if mock_enabled:
+            with open('./mock_data/x2.txt', mode='rt', encoding='utf-8') as f:
+                result_harmonics_x2 = ast.literal_eval(''.join(f.readlines()))
+            with open('./mock_data/x3.txt', mode='rt', encoding='utf-8') as f:
+                result_harmonics_x3 = ast.literal_eval(''.join(f.readlines()))
+        # endregion
+
+        src.send('OUTPut OFF')
+        sa.send(':CAL:AUTO ON')
+
+        return result, result_harmonics_x2, result_harmonics_x3
+
+    def _add_measure_point(self, data):
+        print('measured point:', data)
+        self.result.add_point(data)
+        self.pointReady.emit()
+
+    def saveConfigs(self):
+        pprint_to_file('params.ini', self.secondaryParams.params)
+
+    @pyqtSlot(dict)
+    def on_secondary_changed(self, params):
+        self.secondaryParams.params = params
+
+    @property
+    def status(self):
+        return [i.status for i in self._instruments.values()]
---
+++ measureresult.py
@@ -0,0 +1,327 @@
+import os
+import openpyxl
+import pandas as pd
+
+from collections import defaultdict
+from openpyxl.chart import LineChart, Series, Reference
+from openpyxl.chart.axis import ChartLines
+from openpyxl.cell import Cell
+from textwrap import dedent
+
+from forgot_again.file import load_ast_if_exists, pprint_to_file, make_dirs, open_explorer_at
+from forgot_again.string import now_timestamp
+
+GIGA = 1_000_000_000
+MEGA = 1_000_000
+KILO = 1_000
+MILLI = 1 / 1_000
+
+
+class MeasureResult:
+    device = 'vco'
+    measurement_name = 'tune'
+    path = 'xlsx'
+
+    def __init__(self):
+        self._secondaryParams = dict()
+        self._raw = list()
+        self._raw_x2 = list()
+        self._raw_x3 = list()
+
+        self._report = dict()
+
+        self._processed = list()
+        self._processed_x2 = list()
+        self._processed_x3 = list()
+
+        self.ready = False
+
+        self.data1 = defaultdict(list)
+        self.data2 = defaultdict(list)
+        self.data3 = dict()
+        self.data4 = dict()
+        self.data5 = defaultdict(list)
+        self.data6 = defaultdict(list)
+
+        self.adjustment = load_ast_if_exists('adjust.ini', default=None)
+
+    def __bool__(self):
+        return self.ready
+
+    def _process(self):
+        harm_x2 = [list(d.values()) for d in self._raw_x2]
+        harm_x2 = _find_deltas(harm_x2, self._processed)
+        self.data3[1] = harm_x2
+        self._processed_x2 = harm_x2
+
+        harm_x3 = [list(d.values()) for d in self._raw_x3]
+        harm_x3 = _find_deltas(harm_x3, self._processed)
+        self.data4[1] = harm_x3
+        self._processed_x3 = harm_x3
+
+        self.ready = True
+
+    def add_harmonics_measurement(self, x2, x3):
+        self._raw_x2 = list(x2)
+        self._raw_x3 = list(x3)
+
+    def _process_point(self, data):
+        u_src = data['u_src']
+        u_control = data['u_control']
+
+        f_tune = data['read_f'] / MEGA
+        p_out = data['read_p']
+        i_src = data['read_i'] / MILLI
+
+        if self.adjustment is not None:
+            point = self.adjustment[len(self._processed)]
+            f_tune += point['f_tune']
+            p_out += point['p_out']
+            i_src += point['i_src']
+
+        self._report = {
+            'u_src': u_src,
+            'u_control': u_control,
+            'f_tune': f_tune,
+            'p_out': p_out,
+            'i_src': i_src,
+        }
+
+        self.data1[u_src].append([u_control, f_tune])
+        self.data2[u_src].append([u_control, p_out])
+        self.data5[u_src].append([u_control, i_src])
+
+        if len(self._processed):
+            # (f2 - f1) / (u2 - u1) * 100
+            last_point = self._processed[-1]
+            f1 = last_point['f_tune']
+            f2 = f_tune
+            u1 = last_point['u_control']
+            u2 = u_control
+            tune = (f2 - f1) / (u2 - u1) * 100
+            self.data6[u_src].append([u_control, tune])
+        self._processed.append({**self._report})
+
+    def clear(self):
+        self._secondaryParams.clear()
+        self._raw.clear()
+        self._raw_x2.clear()
+        self._raw_x3.clear()
+
+        self._report.clear()
+
+        self._processed.clear()
+        self._processed_x2.clear()
+        self._processed_x3.clear()
+
+        self.data1.clear()
+        self.data2.clear()
+        self.data3.clear()
+        self.data4.clear()
+        self.data5.clear()
+        self.data6.clear()
+
+        self.adjustment = load_ast_if_exists('adjust.ini', default=None)
+
+        self.ready = False
+
+    def set_secondary_params(self, params):
+        self._secondaryParams = dict(**params.params)
+
+    def add_point(self, data):
+        self._raw.append(data)
+        self._process_point(data)
+
+    def save_adjustment_template(self):
+        if self.adjustment is None:
+            print('measured, saving template')
+            self.adjustment = [{
+                'u_src': p['u_src'],
+                'u_control': p['u_control'],
+                'f_tune': 0,
+                'p_out': 0,
+                'i_src': 0,
+            } for p in self._processed]
+            pprint_to_file('adjust.ini', self.adjustment)
+
+    @property
+    def report(self):
+        return dedent("""        Источник питания:
+        Uпит, В={u_src}
+        Uупр, В={u_control}
+        Iпот, mA={i_src}
+
+        Анализатор:
+        Fвых, МГц={f_tune:0.3f}
+        Pвых, дБм={p_out:0.3f}
+        """.format(**self._report))
+
+    def export_excel(self):
+        make_dirs(self.path)
+        fn = self._secondaryParams.get('file_name', None) or f'{self.device}-{self.measurement_name}-{now_timestamp()}'
+        file_name = f'./{self.path}/{fn}.xlsx'
+
+        u_dr_1, u_dr_2, u_dr_3 = 0, 0, 0
+
+        udrs = list({point['u_src']: 0 for point in self._processed}.keys())
+        try:
+            u_dr_1 = udrs[0]
+            u_dr_2 = udrs[1]
+            u_dr_3 = udrs[2]
+        except IndexError:
+            pass
+
+        result_harmonics_x2 = [[u_dr_1] + row for row in self._processed_x2]
+        result_harmonics_x3 = [[u_dr_1] + row for row in self._processed_x3]
+
+        df_harm_2 = pd.DataFrame(result_harmonics_x2, columns=['Uпит, В', 'Uупр, В', 'Pвых_2, дБм'])
+        df_harm_3 = pd.DataFrame(result_harmonics_x3, columns=['Uпит, В', 'Uупр, В', 'Pвых_3, дБм'])
+
+        df = pd.DataFrame(self._processed)
+        df.columns=['Uпит, В', 'Uупр, В', 'Fвых, МГц', 'Pвых, дБм', 'Iпот, мА', ]
+
+        df['fdiff'] = df.groupby('Uпит, В')['Fвых, МГц'].diff().shift(-1)
+        df['udiff'] = df.groupby('Uпит, В')['Uупр, В'].diff().shift(-1)
+        df['S, МГц/В'] = df[df['fdiff'].notna()].apply(lambda row: (row['fdiff'] / row['udiff']) * 100, axis = 1)
+        df = df.drop(['fdiff', 'udiff'], axis=1)
+
+        df = pd.merge(df, df_harm_2, how='left', on=['Uпит, В', 'Uупр, В'])
+        df = pd.merge(df, df_harm_3, how='left', on=['Uпит, В', 'Uупр, В'])
+
+        df['Pвых_2отн, дБм'] = df[df['Pвых_2, дБм'].notna()].apply(lambda row: -(row['Pвых, дБм'] - row['Pвых_2, дБм']), axis=1)
+        df['Pвых_3отн, дБм'] = df[df['Pвых_3, дБм'].notna()].apply(lambda row: -(row['Pвых, дБм'] - row['Pвых_3, дБм']), axis=1)
+        df['S, МГц/В'] = df['S, МГц/В'].fillna(0)
+
+        df_udr_1 = df[df['Uпит, В'] == u_dr_1]
+        df_udr_2 = df[df['Uпит, В'] == u_dr_2]
+        df_udr_3 = df[df['Uпит, В'] == u_dr_3]
+
+        cols = len(df_udr_1.columns)
+        rows = len(df_udr_1)
+
+        udr_1_s = [df_udr_1.columns.values.tolist()] + df_udr_1.values.tolist()
+        udr_2_s = [df_udr_2.columns.values.tolist()] + (df_udr_2.values.tolist() or [[''] * cols] * rows)
+        udr_3_s = [df_udr_3.columns.values.tolist()] + (df_udr_3.values.tolist() or [[''] * cols] * rows)
+
+        print(udr_1_s)
+        print(udr_2_s)
+        print(udr_3_s)
+
+        wb = openpyxl.Workbook()
+        ws = wb.active
+
+        out = []
+        for r1, r2, r3 in zip(udr_1_s, udr_2_s, udr_3_s):
+            row = r1 + ['', ''] + r2 + ['', ''] + r3
+            out.append(row)
+
+        for row in out:
+            ws.append(row)
+
+        top_left_cell: Cell = ws.cell(row=rows + 4, column=2)
+        dx = 9
+        dy = 15
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows + 1}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!C2:C{rows + 1}'),
+                Reference(ws, range_string=f'{ws.title}!O2:O{rows + 1}'),
+                Reference(ws, range_string=f'{ws.title}!AA2:AA{rows + 1}'),
+            ],
+            title='Диапазон перестройки',
+            loc=top_left_cell.offset(0, 0).coordinate,
+            curve_labels=['Uпит = 4.7В', 'Uпит = 5.0В', 'Uпит = 5.3В'],
+            ax_titles=['Uупр, В', 'Fвых, МГц'],
+        )
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows + 1}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!D2:D{rows + 1}'),
+                Reference(ws, range_string=f'{ws.title}!P2:P{rows + 1}'),
+                Reference(ws, range_string=f'{ws.title}!AB2:AB{rows + 1}'),
+            ],
+            title='Мощность',
+            loc=top_left_cell.offset(0, dx).coordinate,
+            curve_labels=['Uпит = 4.7В', 'Uпит = 5.0В', 'Uпит = 5.3В'],
+            ax_titles=['Uупр, В', 'Pвых, дБм'],
+        )
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows + 1}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!I2:I{rows + 1}'),
+            ],
+            title='Относительный уровень 2й гармоники',
+            loc=top_left_cell.offset(dy, 0).coordinate,
+            curve_labels=['Uпит = 4.7В'],
+            ax_titles=['Uупр, В', 'Pвых х2, МГц'],
+        )
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows + 1}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!J2:J{rows + 1}'),
+            ],
+            title='Относительный уровень 3й гармоники',
+            loc=top_left_cell.offset(dy, dx).coordinate,
+            curve_labels=['Uпит = 4.7В'],
+            ax_titles=['Uупр, В', 'Pвых х3, МГц'],
+        )
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows + 1}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!E2:E{rows + 1}'),
+            ],
+            title='Ток потребления',
+            loc=top_left_cell.offset(0, 2 * dx).coordinate,
+            curve_labels=['Uпит = 4.7В'],
+            ax_titles=['Uупр, В', 'Iпот, мА'],
+        )
+
+        _add_chart(
+            ws=ws,
+            xs=Reference(ws, range_string=f'{ws.title}!B2:B{rows}'),
+            ys=[
+                Reference(ws, range_string=f'{ws.title}!F2:F{rows}'),
+            ],
+            title='Чувствительность',
+            loc=top_left_cell.offset(dy, 2 * dx).coordinate,
+            curve_labels=['Uпит = 4.7В'],
+            ax_titles=['Uупр, В', 'S, МГц/В'],
+        )
+
+        wb.save(file_name)
+        open_explorer_at(os.path.abspath(file_name))
+
+
+def _add_chart(ws, xs, ys, title, loc, curve_labels=None, ax_titles=None):
+    chart = LineChart()
+
+    for y, label in zip(ys, curve_labels):
+        ser = Series(y, title=label)
+        chart.append(ser)
+
+    chart.set_categories(xs)
+    chart.title = title
+
+    chart.x_axis.minorGridlines = ChartLines()
+    chart.x_axis.tickLblPos = 'low'
+
+    if ax_titles:
+        chart.x_axis.title = ax_titles[0]
+        chart.y_axis.title = ax_titles[1]
+    # chart.x_axis.tickLblSkip = 3
+
+    ws.add_chart(chart, loc)
+
+
+def _find_deltas(harm, origin):
+    return [[harm[0], -(main['p_out'] - harm[1])] for harm, main in zip(harm, origin)]
