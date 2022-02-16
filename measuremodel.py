d1d9c9610fc7806bf3bb39e3555b47b2164a559c
igrekus <upload234@mail.ru>
2022-02-16 16:18:54 +0300

    feat: change measure model table format

--- measuremodel.py
+++ measuremodel.py
@@ -1,5 +1,6 @@
 import datetime
 import os
+from collections import defaultdict
 
 from subprocess import Popen
 
@@ -7,26 +8,36 @@ from forgot_again.file import make_dirs
 from pandas import DataFrame
 from PyQt5.QtCore import Qt, QAbstractTableModel, QVariant
 
+from instr.const import GIGA
+
 
 class MeasureModel(QAbstractTableModel):
     def __init__(self, parent=None, header=None):
         super().__init__(parent)
 
         self._header = header or ['#']
-        self._data = list()
+        self._data = defaultdict(dict)
+        self._pows = list()
+        self._freqs = list()
 
     def clear(self):
         self.beginResetModel()
         self._data.clear()
+        self._pows.clear()
+        self._freqs.clear()
         self.endResetModel()
 
     def update(self, point: dict):
+        # a = {'f': 2700000000, 'p': 14.9917657, 'read_pow': 12.7655025, 'adjusted_pow': 15.0660151}
         self.beginResetModel()
-        try:
-            idx = self._data[-1]['idx'] + 1
-        except LookupError:
-            idx = 1
-        self._data.append({'idx': idx, **point})
+
+        p = round(point['p'])
+        f = round(point['f'] / GIGA, 3)
+        self._pows = sorted(set(self._pows + [p]))
+        self._freqs = sorted(set(self._freqs + [f]))
+        self._data[p][f] = (point['read_pow'], point['adjusted_pow'])
+
+        self._header = ['Pвх, дБм'] + [f'Fвх={v}, ГГц' for v in self._freqs]
         self.endResetModel()
 
     def headerData(self, section, orientation, role=None):
@@ -39,7 +50,7 @@ class MeasureModel(QAbstractTableModel):
     def rowCount(self, parent=None, *args, **kwargs):
         if parent.isValid():
             return 0
-        return len(self._data)
+        return len(self._pows)
 
     def columnCount(self, parent=None, *args, **kwargs):
         return len(self._header)
@@ -50,23 +61,12 @@ class MeasureModel(QAbstractTableModel):
         row = index.row()
         col = index.column()
         if role == Qt.DisplayRole:
-            row_data = self._data[row]
+            p = self._pows[row]
             if col == 0:
-                return QVariant(row_data['idx'])
-            elif col == 1:
-                return QVariant(row_data['f'] / 1_000_000_000)
-            elif col == 2:
-                return QVariant(float(row_data['p']))
-            elif col == 3:
-                return QVariant(row_data['read_pow'])
-            elif col == 4:
-                return QVariant(float(row_data['adjusted_pow']))
-            else:
-                QVariant()
-        return QVariant()
+                return QVariant(p)
+            return QVariant(self._data[p].get(self._freqs[col - 1], (0, 0))[1])
 
-    def calData(self):
-        return list(self._data)
+        return QVariant()
 
     def is_ready(self):
         return bool(self._data)
